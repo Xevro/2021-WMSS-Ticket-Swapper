f17b964fd359ba1386d8c2fed587947d786ade6c
Timon De Bruyne <timon.debruyne@student.odisee.be>
2020-11-04 18:19:15 +0100

    VALIDATION
    Validation works, appart from the start- and enddate (work in progress).
    Event.php created.

--- app/public/register-event.php
+++ app/public/register-event.php
@@ -9,6 +9,72 @@ require_once $basePath . 'vendor/autoload.php';
 $loader = new \Twig\Loader\FilesystemLoader($basePath . '/resources/templates');
 $twig = new \Twig\Environment($loader);
 
-// View
+require_once $basePath . 'src/Models/Event.php';
 
-echo $twig->render('pages/register-event.twig', []);
+$eventName = isset($_POST['eventName']) ? (string)$_POST['eventName'] : '';
+$standardPrice = isset($_POST['standardPrice']) ? (string)$_POST['standardPrice'] : '';
+$location = isset($_POST['location']) ? (string)$_POST['location'] : '';
+$description = isset($_POST['description']) ? (string)$_POST['description'] : '';
+$artists = isset($_POST['artists']) ? (string)$_POST['artists'] : '';
+$errorName = '';
+$errorPrice = '';
+$errorLocation = '';
+$errorDescription = '';
+$errorArtists = '';
+
+
+if (isset($_POST['btnRegister'])) {
+    $errors = false;
+    if ($eventName === '') {
+        $errorName = 'Name is required!';
+        $errors = true;
+    }
+
+    if ($standardPrice === '') {
+        $errorPrice = 'Price is required!';
+        $errors = true;
+    }
+
+    if ($location === '') {
+        $errorLocation = 'Location is required!';
+        $errors = true;
+    }
+
+    if ($description === '') {
+        $errorDescription = 'Description is required!';
+        $errors = true;
+    }
+
+    if ($artists === '') {
+        $errorArtists = 'Artists are required!';
+        $errors = true;
+    }
+
+    /*$datum = DateTime::createFromFormat('Y-m-d H:i:s', $enddate);
+    $errors = DateTime::getLastErrors();
+    if (empty($errors['warning_count']) || !empty($errors['error_count'])) {
+        $msgDate = 'Please enter a valid date';
+        $allOK = false;
+    }*/
+
+}
+
+
+
+
+// View
+echo $twig->render('pages/register-event.twig', [
+    'eventName' => $eventName,
+    'standardPrice' => $standardPrice,
+    'location' => $location,
+    'description' => $description,
+    'artists' => $artists,
+    'errorName' => $errorName,
+    'errorPrice' => $errorPrice,
+    'errorLocation' => $errorLocation,
+    'errorDescription' => $errorDescription,
+    'errorArtists' => $errorArtists,
+    'action' => $_SERVER['PHP_SELF']
+]);
+
+
