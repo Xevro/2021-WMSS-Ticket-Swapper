cc4928f8355fc9ef03ff951419c17dace94d4281
Timon De Bruyne <timon.debruyne@student.odisee.be>
2020-11-07 20:14:03 +0100

    Full send to database
    All values are send to database now. Changed 'datetime' to varchar to avoid errors. Need to fix this later.

--- app/public/register-event.php
+++ app/public/register-event.php
@@ -96,8 +96,8 @@ if (isset($_POST['btnRegister'])) {
 
     if ($allOk  && $allOkDateStart && $allOkDateEnd) {
         //add to database
-        $stmt = $connection->prepare('INSERT INTO Evenementen(Naam, standaard_ticketprijs, Locatie, Beschrijving, Aanwezige_artiesten) VALUES (?,?,?,?,?)');
-        $stmt->execute([$eventName, $standardPrice, $location, $description, $artists]);
+        $stmt = $connection->prepare('INSERT INTO Evenementen(Naam, standaard_ticketprijs, Aanvangstijd, Sluitingstijd, Locatie, Beschrijving, Aanwezige_artiesten) VALUES (?,?,?,?,?,?,?)');
+        $stmt->execute([$eventName, $standardPrice, $startDate, $endDate, $location, $description, $artists]);
         header('Location: register-event.php');
         exit();
     }
