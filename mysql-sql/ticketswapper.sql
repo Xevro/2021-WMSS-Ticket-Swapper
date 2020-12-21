774aa6c2a1ee49a34a37768205ea0e301f6f1409
Louis D'Hont <louisdhont@MacBook-Pro-15-van-Louis.local>
2020-12-22 00:37:35 +0100

    Update ticketswapper.sql

--- mysql-sql/ticketswapper.sql
+++ mysql-sql/ticketswapper.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: mysqldb
--- Gegenereerd op: 21 dec 2020 om 23:02
+-- Gegenereerd op: 21 dec 2020 om 23:37
 -- Serverversie: 5.7.32
 -- PHP-versie: 7.4.11
 
@@ -20,6 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Database: `ticketswapper`
 --
+CREATE DATABASE IF NOT EXISTS `ticketswapper` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `ticketswapper`;
 
 -- --------------------------------------------------------
 
@@ -27,7 +29,7 @@ SET time_zone = "+00:00";
 -- Tabelstructuur voor tabel `contact`
 --
 
-CREATE TABLE `contact` (
+CREATE TABLE IF NOT EXISTS `contact` (
   `name` varchar(255) NOT NULL,
   `first_name` varchar(255) NOT NULL,
   `subject` varchar(45) NOT NULL,
@@ -47,8 +49,8 @@ INSERT INTO `contact` (`name`, `first_name`, `subject`, `message`) VALUES
 -- Tabelstructuur voor tabel `events`
 --
 
-CREATE TABLE `events` (
-  `event_id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `events` (
+  `event_id` int(11) NOT NULL AUTO_INCREMENT,
   `event_name` varchar(100) NOT NULL,
   `standard_ticket_price` float(6,2) NOT NULL,
   `start_date` datetime NOT NULL,
@@ -56,8 +58,9 @@ CREATE TABLE `events` (
   `location` varchar(200) NOT NULL,
   `description` varchar(1000) NOT NULL,
   `artist` varchar(1000) NOT NULL,
-  `slug` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `slug` varchar(255) NOT NULL,
+  PRIMARY KEY (`event_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8;
 
 --
 -- Gegevens worden geëxporteerd voor tabel `events`
@@ -86,16 +89,19 @@ INSERT INTO `events` (`event_id`, `event_name`, `standard_ticket_price`, `start_
 -- Tabelstructuur voor tabel `tickets`
 --
 
-CREATE TABLE `tickets` (
-  `ticket_id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `tickets` (
+  `ticket_id` int(11) NOT NULL AUTO_INCREMENT,
   `ticket_name` varchar(100) NOT NULL,
   `ticket_price` float(6,2) NOT NULL,
   `amount` int(11) NOT NULL,
   `reason_for_sell` varchar(255) NOT NULL,
   `ticket_file_location` varchar(300) DEFAULT NULL,
   `events_id_event` int(11) NOT NULL,
-  `users_gebruiker_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `users_gebruiker_id` int(11) NOT NULL,
+  PRIMARY KEY (`ticket_id`),
+  KEY `fk_tickets_events1_idx` (`events_id_event`),
+  KEY `fk_tickets_users1_idx` (`users_gebruiker_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
 
 --
 -- Gegevens worden geëxporteerd voor tabel `tickets`
@@ -113,16 +119,17 @@ INSERT INTO `tickets` (`ticket_id`, `ticket_name`, `ticket_price`, `amount`, `re
 -- Tabelstructuur voor tabel `users`
 --
 
-CREATE TABLE `users` (
-  `gebruiker_id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `users` (
+  `gebruiker_id` int(11) NOT NULL AUTO_INCREMENT,
   `first_name` varchar(50) NOT NULL,
   `last_name` varchar(50) NOT NULL,
   `address` varchar(200) NOT NULL,
   `couponcode` varchar(10) NOT NULL,
   `invite_number` varchar(45) DEFAULT '0',
   `email` varchar(150) NOT NULL,
-  `password` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`gebruiker_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
 
 --
 -- Gegevens worden geëxporteerd voor tabel `users`
@@ -132,52 +139,6 @@ INSERT INTO `users` (`gebruiker_id`, `first_name`, `last_name`, `address`, `coup
 (1, 'Louis', 'D\'Hont', 'Dorp 71A, Oosterzele', 'sD4oFcma9c', '0', 'dhontlouis@gmail.com', '$2y$10$c1sEgmT2nGWGCZXGybKtuuk4Fsw97niUWrfNd2OlPM/dG46g4K4n2'),
 (2, 'Joris', 'Maervoet', 'Gebroeders de Smetstraat 1, 9000 Gent', 'fo2qmt17xr', '0', 'joris.maervoet@odisee.be', '$2y$10$qpMTkVbvc6E.4dt2uk9BlOOadOXiz3J5tqiBwtcmWMupGiX5GiO/q'),
 (3, 'Timon', 'De Bruyne', 'randomStraat 9, 9000 Gent', 'f2TIKVJ2KP', '0', 'timon.debruyne@student.odisee.be', '$2y$10$KqrUuDwLRFHEBX/Ul2wEMOthuLZqRYa0cL0xhYaLcknux7jNAETxO');
-
---
--- Indexen voor geëxporteerde tabellen
---
-
---
--- Indexen voor tabel `events`
---
-ALTER TABLE `events`
-  ADD PRIMARY KEY (`event_id`);
-
---
--- Indexen voor tabel `tickets`
---
-ALTER TABLE `tickets`
-  ADD PRIMARY KEY (`ticket_id`),
-  ADD KEY `fk_tickets_events1_idx` (`events_id_event`),
-  ADD KEY `fk_tickets_users1_idx` (`users_gebruiker_id`);
-
---
--- Indexen voor tabel `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`gebruiker_id`);
-
---
--- AUTO_INCREMENT voor geëxporteerde tabellen
---
-
---
--- AUTO_INCREMENT voor een tabel `events`
---
-ALTER TABLE `events`
-  MODIFY `event_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
-
---
--- AUTO_INCREMENT voor een tabel `tickets`
---
-ALTER TABLE `tickets`
-  MODIFY `ticket_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
-
---
--- AUTO_INCREMENT voor een tabel `users`
---
-ALTER TABLE `users`
-  MODIFY `gebruiker_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- Beperkingen voor geëxporteerde tabellen
