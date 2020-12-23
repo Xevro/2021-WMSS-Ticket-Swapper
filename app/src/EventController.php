c7c4a2d20c89760405ea8c77c8a65038dac914cb
Louis D'Hont <louisdhont@MacBook-Pro-15-van-Louis.local>
2020-12-23 01:41:15 +0100

    Small change

--- app/src/EventController.php
+++ app/src/EventController.php
@@ -387,6 +387,5 @@ class EventController {
     public function error404() {
         header($_SERVER['SERVER_PROTOCOL'] . ' 404 Not Found');
         echo $this->twig->render('pages/error-404.twig');
-
     }
 }
